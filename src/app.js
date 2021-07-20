c8146d3b767b183ad6d946060437299828175610
manikandanramar <manikandanranar@outlook.com>
2021-07-20 22:11:50 +0530

    browser router changed to hash router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch, Link } from 'react-router-dom'
+import { HashRouter as Router, Switch, Link } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import routes from './routes';
 import configureStore from './redux/create';
